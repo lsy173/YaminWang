2a2e2a32aed9e3a5b61e34dc5e035038d7d70b23
sogangCSmaster <cs20131575@u.sogang.ac.kr>
2019-01-06 22:30:01 +0900

    

--- Update.js
+++ Update.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Dimensions, Platform, Keyboard, Alert } from 'react-native';
 import axios from 'axios';
+import config from './config.json';
+
+const { height, width } = Dimensions.get("window");
+const { api, token, channel, username } = config;
 
 export default class Update extends React.Component {
     state={
@@ -10,15 +14,30 @@ export default class Update extends React.Component {
     render() {
         return (
             <View>
-                <Text>여러분의 요청이 앱을 완성시킵니다!</Text>
+                <Text style={{textAlign: "center", fontSize: 17, marginTop: 5, fontWeight: "700"}}>여러분의 요청이 앱을 완성시킵니다!</Text>
                 <TextInput
-                    placeholder={"단어 입력"}
+                    style={styles.search}
+                    underlineColorAndroid={"transparent"}
+                    placeholder={"새 단어 입력"}
                     onChangeText={this._controlWord}
+                    autoCorrect={false}
+                    placeholderTextColor={"#55595C"}
                 />
                 <TextInput
+                    style={styles.search2}
                     placeholder={"뜻 입력"}
                     onChangeText={this._controlExplain}
+                    underlineColorAndroid={"transparent"}
+                    autoCorrect={false}
+                    multiline={true}
+                    placeholderTextColor={"#55595C"}
                 />
+
+                <TouchableOpacity style={{alignItems: "center"}} onPress={this._submit} TouchableOpacity={0.7}>
+                    <View style={styles.button}>
+                        <Text style={{color: "white", fontSize: 17, fontWeight: "700"}}>등록 요청</Text>
+                    </View>
+                </TouchableOpacity>
                 
             </View>
         );
@@ -35,4 +54,71 @@ export default class Update extends React.Component {
             inputExplain: text
         })
     }
+
+    _submit = async() => {
+        var { inputWord, inputExplain } = this.state;
+        Keyboard.dismiss();
+
+        var text = "새 요청\n" + inputWord + "\n\n" + inputExplain;
+        console.warn(api);
+        await axios.get(api, {
+            params: {
+                token: token,
+                channel: channel,
+                username: username,
+                text: text
+            }
+        });
+
+        this.setState({
+            inputWord: "",
+            inputExplain: ""
+        });
+
+
+        Alert.alert("등록 요청 성공", "감사합니다. 성공적으로 등록 요청 되었습니다.");
+    }
 }
+
+const styles = StyleSheet.create({
+    search: {
+        width: width,
+        height: 50,
+        paddingLeft: 17,
+        backgroundColor: "white",
+        color: "black",
+        marginTop: 15
+    },
+    search2: {
+        width: width,
+        paddingLeft: 17,
+        backgroundColor: "white",
+        color: "black",
+        marginTop: 15,
+        paddingBottom: 17,
+        paddingTop: 15
+    },
+    button: {
+        width: width-120,
+        height: 50,
+        backgroundColor: "#55595C",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 20,
+        borderRadius: 10,
+        ...Platform.select({
+            ios: {
+                shadowColor: "rgb(50, 50, 50)",
+                shadowOpacity: 0.5,
+                shadowRadius: 5,
+                shadowOffset: {
+                    height: 1,
+                    width: 1
+                }
+            },
+            android: {
+                elevation: 5
+            }
+        })
+    }
+})
