aee501e152a584dcd01b94ac62631e6ea4343ce2
sogangCSmaster <cs20131575@u.sogang.ac.kr>
2019-01-06 23:00:52 +0900

    add admob

--- Worddetail.js
+++ Worddetail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, Dimensions, WebView } from 'react-native';
+import { AdMobBanner } from 'expo';
 
 const { height, width } = Dimensions.get("window");
 
@@ -9,18 +10,34 @@ export default class Worddetail extends React.Component {
 
         if(url!=""){
             return (
-                <WebView
-                    source={{uri: url}}
-                    style={styles.webView}
-                />
+                <View style={{width: width, height: height-70, alignItems: "center"}}>
+                    <WebView
+                        source={{uri: url}}
+                        style={styles.webView}
+                    />
+                    <AdMobBanner
+                        bannerSize="banner"
+                        adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+                        testDeviceID="EMULATOR"
+                        onDidFailToReceiveAdWithError={this.bannerError}
+                    />
+                </View>
             );
         } else {
             return (
                 <View>
-                    <Text style={{fontSize: 20, textAlign: "center", marginTop: 15}}>{word}</Text>
-                    <ScrollView style={{height: height-100}}>
-                        <Text style={{marginTop: 20, padding: 20}}>{content}</Text>
+                    <Text style={{fontSize: 20, textAlign: "center", marginTop: 15, fontWeight: "700"}}>{word}</Text>
+                    <ScrollView style={{height: 300}}>
+                        <Text style={{marginTop: 10, paddingLeft: 20, paddingRight: 20}}>{content}</Text>
                     </ScrollView>
+                    <View style={{alignItems: "center"}}>
+                        <AdMobBanner
+                            bannerSize="mediumRectangle"
+                            adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
+                            testDeviceID="EMULATOR"
+                            onDidFailToReceiveAdWithError={this.bannerError}
+                        />
+                    </View>
                 </View>
             )
         }
@@ -31,7 +48,6 @@ export default class Worddetail extends React.Component {
 
 const styles = StyleSheet.create({
     webView: {
-        width: width,
-        height: height
+        width: width
     }
 })
