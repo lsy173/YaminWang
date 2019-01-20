f92eb08643191e6d54ac1a01c0c47be89a51ec65
sogangCSmaster <cs20131575@u.sogang.ac.kr>
2019-01-20 11:25:06 +0900

    hmm..

--- Worddetail.js
+++ Worddetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Dimensions, WebView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions, WebView, Platform } from 'react-native';
 import { AdMobBanner } from 'expo';
 
 const { height, width } = Dimensions.get("window");
@@ -15,29 +15,45 @@ export default class Worddetail extends React.Component {
                         source={{uri: url}}
                         style={styles.webView}
                     />
-                    <AdMobBanner
-                        bannerSize="banner"
-                        adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
-                        testDeviceID="EMULATOR"
-                        onDidFailToReceiveAdWithError={this.bannerError}
-                    />
+                    {Platform.ios? (
+                        <AdMobBanner
+                            bannerSize="banner"
+                            adUnitID="ca-app-pub-1263632577876314~7013604910"
+                            onDidFailToReceiveAdWithError={this.bannerError}
+                        />
+                    ) : (
+                        <AdMobBanner
+                            bannerSize="banner"
+                            adUnitID="ca-app-pub-1263632577876314/4712308420"
+                            onDidFailToReceiveAdWithError={this.bannerError}
+                        />
+                    )}
+                    
                 </View>
             );
         } else {
             return (
                 <View>
-                    <Text style={{fontSize: 20, textAlign: "center", marginTop: 15, fontWeight: "700"}}>{word}</Text>
-                    <ScrollView style={{height: 300}}>
+                    <ScrollView style={{height: height}}>
+                        <Text style={{fontSize: 20, textAlign: "center", marginTop: 15, fontWeight: "700"}}>{word}</Text>
                         <Text style={{marginTop: 10, paddingLeft: 20, paddingRight: 20}}>{content}</Text>
+                        <View style={{alignItems: "center", marginTop: 20}}>
+                        {Platform.ios? (
+                            <AdMobBanner
+                                bannerSize="mediumRectangle"
+                                adUnitID="ca-app-pub-1263632577876314~7013604910"
+                                onDidFailToReceiveAdWithError={this.bannerError}
+                            />
+                        ) : (
+                            <AdMobBanner
+                                bannerSize="mediumRectangle"
+                                adUnitID="ca-app-pub-1263632577876314/4712308420"
+                                onDidFailToReceiveAdWithError={this.bannerError}
+                            />
+                        )}
+                            
+                        </View>
                     </ScrollView>
-                    <View style={{alignItems: "center"}}>
-                        <AdMobBanner
-                            bannerSize="mediumRectangle"
-                            adUnitID="ca-app-pub-3940256099942544/6300978111" // Test ID, Replace with your-admob-unit-id
-                            testDeviceID="EMULATOR"
-                            onDidFailToReceiveAdWithError={this.bannerError}
-                        />
-                    </View>
                 </View>
             )
         }
